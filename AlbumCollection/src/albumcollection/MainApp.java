3698bb2f74007222f3c08dddcb6ae641870cdbd8
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-02-10 13:52:08 -0600

    new

--- AlbumCollection/src/albumcollection/MainApp.java
+++ AlbumCollection/src/albumcollection/MainApp.java
@@ -1,16 +1,69 @@
 package albumcollection;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author David Rocha dr1340
  */
 public class MainApp {
+ private static final String DATABASE_FILE_NAME = "database.txt";
 
-    /**
-     * @param args the command line arguments
-     */
+    private static ArrayList<Album> albumCollection = new ArrayList<Album>();
+
     public static void main(String[] args) {
-        // dfthsrth
+
+        readCollectionFromDatabase();
+
+        Album newAlbum = new Album();
+        newAlbum.title = "Blah";
+
+        albumCollection.add(newAlbum);
+
+        writeCollectionToDatabase();
     }
-    
+
+    private static void writeCollectionToDatabase() {
+
+        try {
+
+            FileWriter fileWriter = new FileWriter(DATABASE_FILE_NAME, false);
+
+            for (Album album : albumCollection) {
+                String title = album.title;
+                fileWriter.write(title + "\n");
+            }
+
+            fileWriter.close();
+
+        } catch (Exception ex) {
+
+            System.out.println("Exception: " + ex);
+        }
+    }
+
+    private static void readCollectionFromDatabase() {
+
+        File file = new File(DATABASE_FILE_NAME);
+
+        try {
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Album album = new Album();
+                album.title = line;
+                albumCollection.add(album);
+            }
+
+        } catch (Exception ex) {
+
+            System.out.println("Exception: " + ex);
+        }
+
+    }
 }

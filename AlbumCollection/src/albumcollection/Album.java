96a536b769067fd2af3470ff8a25ccbdf714f2d1
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-02-10 20:50:10 -0600

    new

--- AlbumCollection/src/albumcollection/Album.java
+++ AlbumCollection/src/albumcollection/Album.java
@@ -4,9 +4,9 @@ package albumcollection;
  *
  * @author David Rocha
  */
-    enum MusicGenre{
-        ROCK, RAP, COUNTRY, POP;  
-    }
+enum MusicGenre{
+    ROCK, RAP, COUNTRY, POP;  
+}
 
 public class Album {
     private String title;
@@ -14,11 +14,42 @@ public class Album {
     private int releaseYear;
     private MusicGenre musicGenre;
 
-
-public Album(String title, String artist){
-    this.title = title;
-    this.artist = artist;
-    this.releaseYear = releaseYear;
-    this.musicGenre = musicGenre;
-}
+    public Album(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.releaseYear = releaseYear;
+        this.musicGenre = musicGenre;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getArtist() {
+        return artist;
+    }
+    
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    
+    public Integer releaseYear() {
+        return releaseYear;
+    }
+    
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+    
+    public void setMusicGenre(MusicGenre musicGenre) {
+        this.musicGenre = musicGenre;
+    }
+    
+    public MusicGenre getMusicGenre() {
+        return musicGenre;
+    }
 }

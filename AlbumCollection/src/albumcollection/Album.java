f58f0c88b56f333fa9087aae4352b3ba15321066
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-02-10 19:38:12 -0600

    new

--- AlbumCollection/src/albumcollection/Album.java
+++ AlbumCollection/src/albumcollection/Album.java
@@ -2,8 +2,23 @@ package albumcollection;
 
 /**
  *
- * @author bb&cc
+ * @author David Rocha
  */
+    enum MusicGenre{
+        ROCK, RAP, COUNTRY, POP;  
+    }
+
 public class Album {
-    public String title;
+    private String title;
+    private String artist;
+    private int releaseYear;
+    private MusicGenre musicGenre;
+
+
+public Album(String title, String artist){
+    this.title = title;
+    this.artist = artist;
+    this.releaseYear = releaseYear;
+    this.musicGenre = musicGenre;
+}
 }

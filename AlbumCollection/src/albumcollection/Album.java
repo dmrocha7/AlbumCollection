7c5c2a28b60c57793fb0a287c5160784ad0f38d3
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-02-11 22:37:41 -0600

    new

--- AlbumCollection/src/albumcollection/Album.java
+++ AlbumCollection/src/albumcollection/Album.java
@@ -8,12 +8,22 @@ enum MusicGenre{
     ROCK, RAP, COUNTRY, POP;  
 }
 
-public class Album {
+/**
+ *
+ * 
+ */
+public class Album implements Comparable<Album> {
     private String title;
     private String artist;
     private int releaseYear;
     private MusicGenre musicGenre;
 
+    /**
+     *
+     * @param title
+     * @param artist
+     * @param releaseYear
+     */
     public Album(String title, String artist, int releaseYear){
         this.title = title;
         this.artist = artist;
@@ -21,40 +31,81 @@ public class Album {
         this.musicGenre = musicGenre;
     }
     
+    /**
+     *
+     * @return
+     */
     public String getTitle() {
         return title;
     }
     
+    /**
+     *
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
     
+    /**
+     *
+     * @return
+     */
     public String getArtist() {
         return artist;
     }
     
+    /**
+     *
+     * @param artist
+     */
     public void setArtist(String artist) {
         this.artist = artist;
     }
     
+    /**
+     *
+     * @return
+     */
     public Integer getReleaseYear() {
         return releaseYear;
     }
     
+    /**
+     *
+     * @param releaseYear
+     */
     public void setReleaseYear(Integer releaseYear) {
         this.releaseYear = releaseYear;
     }
     
+    /**
+     *
+     * @param musicGenre
+     */
     public void setMusicGenre(MusicGenre musicGenre) {
         this.musicGenre = musicGenre;
     }
     
+    /**
+     *
+     * @return
+     */
     public MusicGenre getMusicGenre() {
         return musicGenre;
     }
+    
     @Override
     public String toString() {
         return "Title: " + this.title + ", Artist: " + this.artist + ", Year: " +
         this.releaseYear + ", Genre: " + musicGenre;
+    }
+    
+    @Override
+    public int compareTo(Album a) {
+        if(getTitle() == null || a.getTitle() == null) {
+            return 0;       
+        }
+        return getTitle().compareTo(a.getTitle());
     }
 }

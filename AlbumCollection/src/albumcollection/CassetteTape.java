8f49ac893ad8bebcae24b591480252257a1f1f90
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-03-03 21:15:12 -0600

    new

---
+++ AlbumCollection/src/albumcollection/CassetteTape.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albumcollection;
+
+/**
+ *
+ * @author bb&cc
+ */
+public class CassetteTape extends Album{
+
+    public CassetteTape(String artist, String title, int releaseYear) {
+        super(artist, title, releaseYear);
+    }
+    
+}

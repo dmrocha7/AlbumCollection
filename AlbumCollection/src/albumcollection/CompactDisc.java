9f2f0048daa8e68dea29a3cf01fcd11ac233a1e5
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-03-03 21:15:16 -0600

    new

---
+++ AlbumCollection/src/albumcollection/CompactDisc.java
@@ -0,0 +1,15 @@
+
+package albumcollection;
+
+/**
+ *
+ * @author bb&cc
+ */
+public class CompactDisc extends Album
+{
+
+    public CompactDisc(String artist, String title, int releaseYear) {
+        super(artist, title, releaseYear);
+    }
+    
+}

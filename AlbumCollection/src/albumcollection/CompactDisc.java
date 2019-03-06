f09e92e1914618f2869b37d6d35fc38f454354d5
bb&cc <bb&cc@DESKTOP-DML9GT4>
2019-03-05 18:00:59 -0600

    new

--- AlbumCollection/src/albumcollection/CompactDisc.java
+++ AlbumCollection/src/albumcollection/CompactDisc.java
@@ -1,15 +1,31 @@
 
 package albumcollection;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author bb&cc
  */
-public class CompactDisc extends Album
+public class CompactDisc extends Album implements Album.playable
 {
-
-    public CompactDisc(String artist, String title, int releaseYear) {
+    ArrayList<Tracks> tracks = new ArrayList<Tracks>();
+    private int curTrack = 0;
+    public CompactDisc(String artist, String title, int releaseYear, 
+                       int currentTrackIndex) {
         super(artist, title, releaseYear);
+                
     }
     
+    public void play() { 
+        
+    }
+    
+    public void fastForward() {
+        
+    }
+    
+    public void rewind() {
+        
+    }
 }
